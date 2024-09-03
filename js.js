1394a90d2113ef0e3e88eedd8b3613bf8f9d88a7
RaizenKS <dieguinhoxx6@gmail.com>
2024-09-03 14:04:28 -0300

    Finish Game

--- js.js
+++ js.js
@@ -1,13 +1,16 @@
 class TicTacToe{
     constructor(){
+        this.audio = document.querySelector('audio');
         this.menuPlayer = document.querySelector("#menu-player");
         this.buttonsMenuPlayer = document.querySelectorAll("#menu-player button");
         this.boxTicTacToe = document.querySelector("#ticTacToe");
         this.boxRow = document.querySelectorAll('.row');
         this.overlay = document.querySelector('#overlay');
-        this.menu = document.querySelector("#menu");
-        this.message = document.querySelector('#menu h2');
-        this.btnRestart = document.querySelector('#menu button');
+        this.menu = document.querySelector("#menu-over");
+        this.message = document.querySelector('#menu-over h2');
+        this.btnRestart = document.querySelector('#menu-over button');
+        this.buttonConfig = document.querySelector(".icon-gear");
+        this.menuVol = document.querySelector('#menu');
         this.robot = false;
         this.turnPlayer = 1;
         this.boxWin = [
@@ -24,8 +27,27 @@ class TicTacToe{
         if(event.target.id == 'btn-2player'){this.robot == false};
         if(event.target.id == 'btn-1player-robot'){this.robot = true};
     }
+
+    openMenu(){
+        this.overlay.style.display = 'flex';
+        this.menuVol.style.display = 'flex';
+
+        document.querySelector(".icon-x").addEventListener('click', (event) => {
+            this.overlay.style.display = 'none';
+            this.menuVol.style.display = 'none';
+        })
+
+        document.querySelector('#menu button').addEventListener('click', ()=>{
+            this.restart();
+        })
+
+        document.querySelector('#menu #inp-range').addEventListener('change',()=>{
+            this.audio.volume = `${document.querySelector('#menu #inp-range').value/100}`    
+        })
+      
+    }
     
-    play(event){
+    async play(event){
         if(event.target.textContent === ''){
             if(this.turnPlayer == 1){
                 event.target.classList.add('x')
@@ -33,7 +55,7 @@ class TicTacToe{
                 this.turnPlayer = 2;
                 this.checkWin();
                 if(this.turnPlayer == 2 && this.robot){
-                    this.makeRobotMove();
+                    await this.makeRobotMove();
                 }
             }else if(this.turnPlayer == 2 && !this.robot){
                 event.target.classList.add('o')
@@ -44,7 +66,6 @@ class TicTacToe{
         }  
     }
 
-
     checkWin(){
         for (let i = 0; i < this.boxWin.length; i++){
             const win = this.boxWin[i]; 
@@ -53,22 +74,24 @@ class TicTacToe{
                 this.menu.style.display = 'flex';
                 this.overlay.style.display = 'flex';
                 this.message.textContent = 'Player 1 venceu'
-                return
+                return 
             }else if(elements.every(element => element.textContent == 'O')){
                 this.menu.style.display = 'flex';
                 this.overlay.style.display = 'flex';
                 this.message.textContent = 'Player 2 venceu'
-                return
-            }else if(Array.from(this.boxRow).every(box => box.textContent !== '')){
-                this.menu.style.display = 'flex';
-                this.overlay.style.display = 'flex';
-                this.message.textContent = 'Empate'
-                return
+                return 
             }
+        }
+        if(Array.from(this.boxRow).every(box => box.textContent !== '')){
+            this.menu.style.display = 'flex';
+            this.overlay.style.display = 'flex';
+            this.message.textContent = 'Empate';
+            return
         }
     }
 
-    makeRobotMove(){
+    async makeRobotMove(){
+        await new Promise(resolve => setTimeout(resolve, 1000));
         let array = Array.from(this.boxRow).filter(row => row.textContent == '')
         if(array.length > 0){
             let random = parseInt(Math.random()*array.length)
@@ -88,6 +111,7 @@ class TicTacToe{
         })
         this.overlay.style.display = 'none';
         this.menu.style.display = 'none';
+        this.menuVol.style.display = 'none';
         this.boxTicTacToe.style.display = 'none';
         this.menuPlayer.style.display = 'flex';
         this.robot = false;
@@ -98,6 +122,7 @@ class TicTacToe{
 // Instancia do jogo
 const game = new TicTacToe
 
+game.audio.volume = '0.3';
 game.buttonsMenuPlayer.forEach((btn)=>{
     btn.addEventListener('click', game.chooseEnemy.bind(game))
 })
@@ -108,9 +133,4 @@ for(let i = 0; i<game.boxRow.length; i++){
 
 game.btnRestart.addEventListener('click', game.restart.bind(game));
 
-
-
-
-
-
-
+game.buttonConfig.addEventListener('click', game.openMenu.bind(game));
